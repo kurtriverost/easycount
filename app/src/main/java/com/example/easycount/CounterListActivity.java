f4c7824c6232e2088c02f3e8c11317562055ab1b
kurtriverost <kurt.riveros@redcomercio.cl>
2023-07-03 22:00:02 -0400

    updated profile selector flow

--- app/src/main/java/com/example/easycount/CounterListActivity.java
+++ app/src/main/java/com/example/easycount/CounterListActivity.java
@@ -2,13 +2,26 @@ package com.example.easycount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CounterListActivity extends AppCompatActivity {
+
+    private String profileName;
+    private TextView profileNameLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_counter_list);
+        Intent intent = getIntent();
+        profileName = intent.getStringExtra("profileName");
+        setViewElements();
+    }
+
+    public void setViewElements(){
+        profileNameLabel = findViewById(R.id.profile_name_label);
+        profileNameLabel.setText(profileName);
     }
 }
--- app/src/main/java/com/example/easycount/ProfileSelectorActivity.java
+++ app/src/main/java/com/example/easycount/ProfileSelectorActivity.java
@@ -1,16 +1,30 @@
 package com.example.easycount;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ProfileSelectorActivity extends AppCompatActivity {
 
     private final String TAG = ProfileSelectorActivity.class.getSimpleName();
-    private Boolean isFirstProfileEmpty = true;
+    private boolean[] areProfilesEmpty = { true, true, true, true };
+
+    private EditText profileNameEditText;
+    private Button firstProfileButton;
+    private Button secondProfileButton;
+    private Button thirdProfileButton;
+    private Button fourthProfileButton;
 
     private static final int WRITE_EXTERNAL_STORAGE = 101;
     private static final String USER_INFO  = "USER_INFO";
@@ -18,14 +32,13 @@ public class ProfileSelectorActivity extends AppCompatActivity {
     private static final String USER_INFO_EMAIL  = "USER_INFO_EMAIL";
     private static final String USER_INFO_COMPANIES = "USER_INFO_COMPANIES";
     private static final String USER_INFO_ROLE = "USER_INFO_ROLE";
-    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_selector);
-
-
+        setViewElements();
+        setListeners();
     }
 
     @Override
@@ -37,5 +50,81 @@ public class ProfileSelectorActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    public void setViewElements(){
+        firstProfileButton = findViewById(R.id.first_profile_btn);
+        secondProfileButton = findViewById(R.id.second_profile_btn);
+        thirdProfileButton = findViewById(R.id.third_profile_btn);
+        fourthProfileButton = findViewById(R.id.fourth_profile_btn);
+    }
+
+    private void setListeners(){
+        firstProfileButton.setOnClickListener(v -> {
+            if (areProfilesEmpty[0]){
+                showEnterProfileNameDialog();
+            } else {
+                Intent intent = new Intent(this, CounterListActivity.class);
+                intent.putExtra("profileName", "perfil 1");
+                startActivity(intent);
+            }
+        });
+
+        secondProfileButton.setOnClickListener(v -> {
+            if (areProfilesEmpty[1]){
+                showEnterProfileNameDialog();
+            } else {
+                Intent intent = new Intent(this, CounterListActivity.class);
+                intent.putExtra("profileName", "perfil 2");
+                startActivity(intent);
+            }
+        });
+
+        thirdProfileButton.setOnClickListener(v -> {
+            if (areProfilesEmpty[2]){
+                showEnterProfileNameDialog();
+            } else {
+                Intent intent = new Intent(this, CounterListActivity.class);
+                intent.putExtra("profileName", "perfil 3");
+                startActivity(intent);
+            }
+        });
+
+        fourthProfileButton.setOnClickListener(v -> {
+            if (areProfilesEmpty[3]){
+                showEnterProfileNameDialog();
+            } else {
+                Intent intent = new Intent(this, CounterListActivity.class);
+                intent.putExtra("profileName", "perfil 4");
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void showEnterProfileNameDialog(){
+        final Dialog discountDialog = new Dialog(ProfileSelectorActivity.this);
+        discountDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        discountDialog.setContentView(R.layout.dialog_enter_new_profile_name);
+        discountDialog.setCanceledOnTouchOutside(false);
+        discountDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        profileNameEditText = discountDialog.findViewById(R.id.dialog_profile_name_edit_text);
+        Button okButton = discountDialog.findViewById(R.id.dialog_ok_button);
+        Button cancelButton = discountDialog.findViewById(R.id.dialog_cancel_button);
+
+
+
+        okButton.setOnClickListener(view -> {
+            String profileName = profileNameEditText.getText().toString();
+            Intent intent = new Intent(this, CounterListActivity.class);
+            intent.putExtra("profileName", "profileName");
+            startActivity(intent);
+        });
+
+
+        cancelButton.setOnClickListener(view -> {
+            discountDialog.dismiss();
+        });
+        discountDialog.show();
     }
 }
